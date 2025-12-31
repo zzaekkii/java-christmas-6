8de8b1fd82ab1bfb4e598853f76389b5752bd4bf
zakie <realzzaekkii@gmail.com>
2025-12-31 15:11:30 +0900

    feat: 주문 입력 데이터에서 메뉴와 개수 추출 구현

--- src/main/java/christmas/controller/PromotionController.java
+++ src/main/java/christmas/controller/PromotionController.java
@@ -1,11 +1,12 @@
 package christmas.controller;
 
+import christmas.Order.Order;
 import christmas.menu.Menu;
-import christmas.menu.MenuType;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static christmas.menu.MenuType.*;
 
@@ -20,17 +21,11 @@ public class PromotionController {
         // 기본 환영 문구 출력
         welcome();
 
-        // 사용자 입력(예약일, 주문)
-        int orderDate = readAndValidateDate();
-        String orderMenu = readAndValidateOrder();
-
+        // 입력한대로 주문 생성
+        Order custumerOrder = readCustomerOrder();
 
         /// TODO: 기본 메뉴 등록
         initializeMenu();
-
-        /// TODO: 주문 메뉴와 개수 추출
-        extractOrderMenu();
-
 
         /// TODO: 결과 출력
 
@@ -38,6 +33,24 @@ public class PromotionController {
 
     private void welcome() {
         outputView.printWelcome();
+    }
+
+    private Order readCustomerOrder() {
+        // 사용자 입력(예약일, 주문)
+        int orderDate = readAndValidateDate();
+
+        while (true) {
+            try {
+                String orderMenu = readAndValidateOrder();
+
+                // 주문 메뉴, 개수 추출
+                Map<Menu, Integer> orderList = extractOrderMenu(orderMenu);
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+
+//        return new Order(orderDate, );
     }
 
     private int readAndValidateDate() {
@@ -60,6 +73,60 @@ public class PromotionController {
         }
     }
 
+    private Map<Menu, Integer> extractOrderMenu(String input) {
+        // , 구분자로 먼저 분리
+        String[] orders = input.split(",");
+
+        // - 구분자로 분리
+        Map<Menu, Integer> orderMenus = new HashMap<>();
+        for (String order : orders) {
+            if (order.isBlank()) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            String[] menuAndCount = order.split("-");
+
+            if (menuAndCount.length != 2) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            // 메뉴 형식에 맞지 않는 경우
+            if (!menuAndCount[0].matches("[A-Za-z]+")) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+            if (!menuAndCount[1].matches("\\d+")) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+
+            // 없는 메뉴를 입력한 경우
+            if (!menuList.containsKey(menuAndCount[0])) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            Menu orderMenu = menuList.get(menuAndCount[0]);
+
+            // 중복된 메뉴를 입력한 경우
+            if (orderMenus.containsKey(orderMenu)) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+
+            // 메뉴 개수가 양수가 아닌 경우
+            try {
+                int count = Integer.parseInt(menuAndCount[1]);
+                orderMenus.put(menuList.get(menuAndCount[0]), count);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
+
+        if (orderMenus.isEmpty()) {
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        return orderMenus;
+    }
+
     private void initializeMenu() {
         // 애피타이저
         menuList.put("양송이수프", new Menu("양송이수프", 6000, APPETIZER));
@@ -80,8 +147,5 @@ public class PromotionController {
         menuList.put("제로콜라", new Menu("제로콜라", 3000, DRINK));
         menuList.put("레드와인", new Menu("레드와인", 60000, DRINK));
         menuList.put("샴페인", new Menu("샴페인", 25000, DRINK));
-    }
-
-    private void extractOrderMenu() {
     }
 }
